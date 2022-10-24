05e02fbb49ecae78a7c20978c250739a9ac76393
karolarizag <karolarizadev@gmail.com>
2022-10-24 21:39:07 +0100

    movie-card restyled and animation

--- src/Components/MovieCard/index.js
+++ src/Components/MovieCard/index.js
@@ -4,28 +4,30 @@ import { parseDate } from '../../Services/Utils'
 export const MovieCard = ({ movie }) => {
 
   const img = `${process.env.REACT_APP_API_IMG}${movie.poster_path}`
-  // Cut the title so it doesn't see too large on the card
   const movieTitle = movie.title.length > 25 ? `${movie.title.slice(0, 25)}...` : movie.title
-  // Format date 
-  const { movieDate } = parseDate(movie.release_date)
-  const popularity = movie.popularity.toString().split('.')[0]
 
   return (
-    <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-5">
-      <img class="rounded-t-lg" src={img} alt="movie-poster" />
-      <div class="p-5">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movieTitle}</h5>
-        <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">{movieDate}</span>
-        <span class="bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">Popularity: {popularity}</span>
-        <div>
-          <Link to={`/${movie.id}`}>
-            <button class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5">
-              See details
-              <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </button>
-          </Link>
+    <div class="movie-card">
+      <Link to={`/${movie.id}`}>
+        <img class="card-front" src={img} alt="movie-poster" />
+        <div class="card-hover card-back p-5">
+          <h5>{movieTitle}</h5>
+          <div>
+            <span class="badge bg-accent">
+              <svg 
+              className="w-6 h-6" 
+              fill="currentColor" 
+              viewBox="0 0 20 20" 
+              xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+              </svg>{movie.vote_average}
+            </span>
+          </div>
+          <div class="mb-2">
+            <span class={`badge bg-alert ${movie.adult ? 'bg-alert' : 'bg-success'}`}>{movie.adult ? '+18' : 'Family content'}</span>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      primary: '#BA2C1C',
+      secondary: '#992C1C',
+      white: '#eee',
+      base: '#181818',
+      accent: '#FFED86', 
+      alert: '#FF2F24',
+      success: '#71E680',
+    }
   },
   plugins: [
     require('flowbite/plugin')
